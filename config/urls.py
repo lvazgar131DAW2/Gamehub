cf247391b6eaf3e627b6cf3c1083cda22bc4ff9e
lvazgar131DAW2 <lvazgar131@fp.iesromerovargas.com>
2026-05-19 14:24:45 +0200

    media cloudinary

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -10,5 +8,3 @@ urlpatterns = [
     path('resenas/', include('reviews.urls')),
     path('usuarios/', include('users.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
